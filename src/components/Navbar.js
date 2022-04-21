d7af920e002f9224d91e0b44844d51c0c13fd970
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-04-21 17:50:40 -0400

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,7 @@
-import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Navbar = () => {
-  const [active, setActive] = useState('active')
   return (
     <Wrapper>
       <div className='logo'></div>
